43e7b83b286332acf21177d689b079b78522dfe5
Biihuy <biihuy25697@gmail.com>
2022-05-19 14:43:20 +0700

    update

--- src/App.js
+++ src/App.js
@@ -3,106 +3,49 @@ import { Formik } from "formik";
 import "./App.css";
 
 export default function App() {
-    const REGEX = {
-        email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    const carList = ["mvw", "ben", "maybach", "mercerdes"];
+    const colorList = ["red", "blue", "white", "green"];
+    const [selectedCar, setSelectedCar] = React.useState({
+        car: "",
+        color: "",
+    });
+
+    const handleChange = (e) => {
+        setSelectedCar({ ...selectedCar, [e.target.name]: e.target.value });
     };
-
-    const [form, setForm] = useState({});
-
-    function handleChange(event) {
-        setForm({
-            ...form,
-            [event.target.name]: event.target.value,
-        });
-    }
-
-    function handleValidate() {
-        const errors = {};
-        if (!form.name) {
-            errors.name = "Required";
-        }
-        if (!form.email) {
-            errors.email = "Required";
-        } else if (!REGEX.email.test(form.email)) {
-            errors.email = "Invalid email address";
-        }
-        if (!form.password) {
-            errors.password = "Required";
-        }
-        if (!form.phone) {
-            errors.phone = "Required";
-        }
-        return errors;
-    }
-
-    function handleSubmit() {
-        alert("Login in successfully!!!");
-        console.log("first");
-    }
-
     return (
         <div>
-            <h1>Contact form</h1>
-            <Formik
-                initialValues={form}
-                validate={handleValidate}
-                onSubmit={handleSubmit}
+            <h1>[Bài tập] Xây dựng component Car selection</h1>
+            <select
+                name="car"
+                defaultValue={"chose car"}
+                onChange={(e) => handleChange(e)}
             >
-                {({ errors, handleSubmit }) => (
-                    <form onSubmit={handleSubmit}>
-                        <div
-                            className={`custom-input ${
-                                errors.name ? "custom-input-error" : ""
-                            }`}
-                        >
-                            <label>Name</label>
-                            <input
-                                type="text"
-                                name="name"
-                                value={form.name || ""}
-                                onChange={handleChange}
-                            />
-                            <p className="error">{errors.name}</p>
-                        </div>
-                        <div
-                            className={`custom-input ${
-                                errors.email ? "custom-input-error" : ""
-                            }`}
-                        >
-                            <label>Email</label>
-                            <input
-                                type="email"
-                                name="email"
-                                value={form.email || ""}
-                                onChange={handleChange}
-                            />
-                            <p className="error">{errors.email}</p>
-                        </div>
-
-                        <div
-                            className={`custom-input ${
-                                errors.phone ? "custom-input-error" : ""
-                            }`}
-                        >
-                            <label>Phone</label>
-                            <input
-                                type="number"
-                                name="phone"
-                                value={form.phone || ""}
-                                onChange={handleChange}
-                            />
-                            <p className="error">{errors.phone}</p>
-                        </div>
-                        <textarea
-                            name="msg"
-                            value={form.msg || ""}
-                            onChange={handleChange}
-                        />
-
-                        <button type="submit">Submit</button>
-                    </form>
-                )}
-            </Formik>
+                <option value={"chose car"} disabled={true}>
+                    chose car
+                </option>
+                {carList.length > 1 &&
+                    carList.map((val, ind) => (
+                        <option key={ind} value={val}>
+                            {val}
+                        </option>
+                    ))}
+            </select>
+            <select
+                name="color"
+                defaultValue={"chose color"}
+                onChange={(e) => handleChange(e)}
+            >
+                <option value={"chose color"} disabled={true}>
+                    chose color
+                </option>
+                {colorList.length > 1 &&
+                    colorList.map((val, ind) => (
+                        <option key={ind} value={val}>
+                            {val}
+                        </option>
+                    ))}
+            </select>
         </div>
     );
 }
